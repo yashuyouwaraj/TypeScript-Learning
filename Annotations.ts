3fc9a45121f3ecba55bd32c71b8ee73a386e1eb0
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-09 21:09:52 +0530

    TypeScript Annotations

---
+++ Annotations.ts
@@ -0,0 +1,13 @@
+//Strings
+let myName: string = "Yashu";
+myName = "Hello Aman";
+console.log(myName);
+
+//Numbers
+let favNumber: number = 8;
+console.log(favNumber);
+
+//Boolean
+let isHard: boolean = false;
+isHard = true;
+console.log(isHard);
