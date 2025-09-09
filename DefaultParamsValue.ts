cf45bcc475fd95766bfb32ecc457a8e8b04baedf
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-09 21:33:08 +0530

    Functions , Default Params

---
+++ DefaultParamsValue.ts
@@ -0,0 +1,5 @@
+function greet(person = "Anonymous"){
+    return `Greetings ${person}`;
+}
+console.log(greet());
+console.log(greet('Yashu'));
---
+++ Functions.ts
@@ -0,0 +1,12 @@
+//Regular Func
+function addOne(num: number){
+    return num+1
+}
+
+const res= addOne(5)
+console.log(res)
+
+//Arrow Func
+const double = (x: number,y: number) =>x*y
+const ans = double(5,6)
+console.log(ans)
