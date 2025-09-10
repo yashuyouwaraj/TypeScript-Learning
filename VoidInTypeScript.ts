28a87100fe78388e8c25ede1cf4f4d8d4dd3eb66
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-10 09:21:17 +0530

    Void

---
+++ VoidInTypeScript.ts
@@ -0,0 +1,9 @@
+function printMessage(message:string):void {
+    console.log(`This is my ${message}`);
+}
+printMessage("Message")
+
+function printMessage2(message:string) {
+    console.log(`This is my ${message}`);
+}
+printMessage2("Message")
