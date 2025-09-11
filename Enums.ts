7cfbb4daeb02ed3ba6bc32a2c63caad0edb77258
Yashu Youwaraj <amanyasu@gmail.com>
2025-09-11 07:58:14 +0530

    Enum

---
+++ Enums.ts
@@ -0,0 +1,10 @@
+enum WeatherConditions{
+    Sunny="Sunny",
+    Rainy="Rainy",
+    Cloudy="Cloudy",
+    Windy="Windy",
+    Snowy="Snowy"
+}
+
+const currentWeather=WeatherConditions.Sunny;
+console.log(`The current weather is ${currentWeather}`);
